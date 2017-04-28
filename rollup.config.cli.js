f783221a5a4d48f4ca402fced69982449012d374
Nathan Cahill <nathan@nathancahill.com>
2017-04-28 12:28:50 -0700

    port rollup to rollup-multiple

---
+++ rollup.config.cli.js
@@ -0,0 +1,33 @@
+import buble from 'rollup-plugin-buble';
+import json from 'rollup-plugin-json';
+import string from 'rollup-plugin-string';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+	entry: 'bin/src/index.js',
+	dest: 'bin/rollup-multiple',
+	format: 'cjs',
+	banner: '#!/usr/bin/env node',
+	plugins: [
+		string({ include: '**/*.md' }),
+		json(),
+		buble(),
+		commonjs({
+			include: 'node_modules/**'
+		}),
+		nodeResolve({
+			main: true
+		})
+	],
+	external: [
+		'fs',
+		'path',
+		'module',
+		'source-map-support',
+		'rollup'
+	],
+	paths: {
+		rollup: '../dist/rollup.js'
+	}
+};
---
+++ src/utils/relativeId.js
@@ -0,0 +1,6 @@
+import { isAbsolute, relative } from './path.js';
+
+export default function relativeId ( id ) {
+    if ( typeof process === 'undefined' || !isAbsolute( id ) ) return id;
+    return relative( process.cwd(), id );
+}
